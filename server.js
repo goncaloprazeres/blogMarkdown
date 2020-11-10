920d330489a2b89c0c03201441a2269f265be1cb
goncaloprazeres <49531024+goncaloprazeres@users.noreply.github.com>
2020-11-10 15:43:37 +0000

    Fisrt part

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+
+
+app.get('/',(req,res)=>{
+    const articles = [{
+        title : 'Test article',
+        createdAt:  new Date(),
+        description: 'Test description'
+    }]
+    res.render('articles/index',{articles : articles})
+})
+
+
+app.use('/articles',articleRouter)
+app.listen(3000)
